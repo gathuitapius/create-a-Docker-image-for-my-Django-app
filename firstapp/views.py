bfe2293c7722a3f50671abfcbb62a56f5d1fe7fb
gathuitapius <gathuitapius097@gmail.com>
2024-01-08 06:59:39 -0500

    create a Docker image of my app

---
+++ firstapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, HttpResponse
+from datetime import date
+
+
+def index(request):
+    template = "<html>"\
+                "This is your first view" \
+                "</html>"
+
+    return HttpResponse(content=template)
+
+
+
+
+def get_date(request):
+    today = date.today()
+    template = "<html>" \
+                "Today's date is {}" \
+                "</html>".format(today)
+
+    return HttpResponse(content=template)
+
+
+
+
+
+
+
+
